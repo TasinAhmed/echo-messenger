70b5aa9e271bfc00d37adbd8ccf7bb61b9487508
Tasin Ahmed <atasin1998@gmail.com>
2025-04-11 21:38:21 -0400

    Remove navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import ChatDetails from "@/components/ChatDetails";
 import Main from "@/components/Main";
-import Navbar from "@/components/Navbar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
